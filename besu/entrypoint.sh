eba6dc1a09fb08072f2d9d72f2f8f33f7947adc2
dappnodedev <diego@dappnode.io>
2024-07-22 17:25:30 +0200

    Use latest staker scripts version

--- besu/entrypoint.sh
+++ besu/entrypoint.sh
@@ -5,19 +5,17 @@ SUPPORTED_NETWORKS="holesky mainnet"
 # shellcheck disable=SC1091
 . /etc/profile
 
-run_client() {
-  echo "[INFO - entrypoint] Running client"
+JWT_PATH=$(get_jwt_path "${NETWORK}" "${SUPPORTED_NETWORKS}" "${SECURITY_PATH}")
 
-  # shellcheck disable=SC2086
-  exec besu --rpc-ws-enabled="${WS_ENABLED}" \
-    --engine-jwt-secret="${JWT_PATH}" \
-    --data-storage-format="${STORAGE_FORMAT}" \
-    --sync-mode="${SYNC_MODE}" \
-    --rpc-http-max-active-connections="${MAX_HTTP_CONNECTIONS}" \
-    --p2p-port="${P2P_PORT}" \
-    --network="${NETWORK}" ${EXTRA_OPTS}
-}
+post_jwt_to_dappmanager "${JWT_PATH}"
 
-set_execution_config_by_network "${SUPPORTED_NETWORKS}"
-post_jwt_to_dappmanager
-run_client
+echo "[INFO - entrypoint] Running client"
+
+# shellcheck disable=SC2086
+exec besu --rpc-ws-enabled="${WS_ENABLED}" \
+  --engine-jwt-secret="${JWT_PATH}" \
+  --data-storage-format="${STORAGE_FORMAT}" \
+  --sync-mode="${SYNC_MODE}" \
+  --rpc-http-max-active-connections="${MAX_HTTP_CONNECTIONS}" \
+  --p2p-port="${P2P_PORT}" \
+  --network="${NETWORK}" ${EXTRA_OPTS}
